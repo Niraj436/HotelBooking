5db553543665d3b61bf4524f472ed1ca711c4c87
niraj436 <nirajkhadka634@gmail.com>
2024-11-12 16:22:13 +0545

    mail chanage

--- server/Utils/emailSender.js
+++ server/Utils/emailSender.js
@@ -1,38 +1,48 @@
-"use strict";
-import nodemailer from "nodemailer";
-import dotenv from "dotenv"
-dotenv.config()
+'use strict';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMPT_HOST,
-  port: process.env.SMPT_PORT,
-//   secure: true,
-  auth: {
-    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-    user: process.env.SMPT_USERNAME,
-    pass: process.env.SMPT_PASSWORD,
-  },
+	/* for testing */
+	// host: process.env.SMPT_HOST,
+	// port: process.env.SMPT_PORT,
+	// //   secure: true,
+	// auth: {
+	// 	// TODO: replace `user` and `pass` values from <https://forwardemail.net>
+	// 	user: process.env.SMPT_USERNAME,
+	// 	pass: process.env.SMPT_PASSWORD,
+	// },
+	/* for email */
+	service: 'gmail',
+	host: 'smtp.gmail.com',
+	port: 587,
+	secure: false, // Use `true` for port 465, `false` for all other ports
+	auth: {
+		user: process.env.USER_EMAIL,
+		pass: process.env.EMAIL_PASS,
+	},
 });
 
 // async..await is not allowed in global scope, must use a wrapper
 async function sendEmail(mailOptions) {
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: mailOptions.from, // sender address
-    to: mailOptions.to, // list of receivers
-    subject:mailOptions.subject, // Subject line
-    text: mailOptions.text, // plain text body
-    html: mailOptions.html, // html body
-  });
+	// send mail with defined transport object
+	const info = await transporter.sendMail({
+		from: mailOptions.from, // sender address
+		to: mailOptions.to, // list of receivers
+		subject: mailOptions.subject, // Subject line
+		text: mailOptions.text, // plain text body
+		html: mailOptions.html, // html body
+	});
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+	console.log('Message sent: %s', info.messageId);
+	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  //
-  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-  //       <https://github.com/forwardemail/preview-email>
-  //
+	//
+	// NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+	//       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+	//       <https://github.com/forwardemail/preview-email>
+	//
 }
 
-export default sendEmail
+export default sendEmail;
