fe1d86c51a341bf97b26f96d1adee09998cdbf5a
niraj436 <nirajkhadka634@gmail.com>
2024-11-12 16:40:49 +0545

    env update

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,5 +1,4 @@
+export const API = import.meta.env.VITE_BACKEND_API;
 
-export const API =  import.meta.env.VITE_BACKEND_API
-export const STRIPE_KEY = import.meta.env.VITE_STRIPE_KEY
-
-
+export const FRONTEND_API = import.meta.env.VITE_FRONTEND_API;
+export const STRIPE_KEY = import.meta.env.VITE_STRIPE_KEY;
